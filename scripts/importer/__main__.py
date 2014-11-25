2f8a131bd356cf5f1f59b1436dd04218f4c231d7
Nico Geyso <nico.geyso@fu-berlin.de>
2014-11-25 21:14:18 +0100

    Added argparse to importer

--- scripts/importer/__main__.py
+++ scripts/importer/__main__.py
@@ -1,18 +1,24 @@
 # -*- coding: utf-8 -*-
 
 import json
+import argparse
 from .importer import Importer
 
-HOST = 'ftp.dwd.de'
-DIR_BASE = 'pub/CDC/observations_germany/climate/'
-#DAILY_HISTORIC = DIR_BASE + 'daily/kl/historical/'
-DAILY_RECENT   = DIR_BASE + 'daily/kl/recent/'
+DEFAULT_HOST = 'ftp.dwd.de'
+DEFAULT_DIR = 'pub/CDC/observations_germany/climate/daily/kl/recent/'
 DEFAULT_FILE_NAME = 'weather.json'
 
-importer = Importer(HOST, DAILY_RECENT)
+parser = argparse.ArgumentParser()
+parser.parse_args()
+parser.add_argument("host", help="ftp host url", nargs='?', default=DEFAULT_HOST)
+parser.add_argument("dir", help="directory", nargs='?', default=DEFAULT_DIR)
+parser.add_argument("file", help="file name", nargs='?', default=DEFAULT_FILE_NAME)
+args = parser.parse_args()
+
+importer = Importer(args.host, args.dir)
 for station in importer.do_import():
      print("%s: %d" % (station.name, len(station.measurements)))
 
-with open(DEFAULT_FILE_NAME, 'w') as f:
-     data = map(lambda x: x.to_dict(), importer.measurements)
+with open(args.file, 'w') as f:
+     data = map(lambda x: x.to_dict(), importer.stations)
      json.dump(data, f, sort_keys = True, indent = 4, ensure_ascii=False)

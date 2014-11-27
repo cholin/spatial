38345173e3389a22ddac8918f1fe6a334d844bcc
Nico Geyso <nico.geyso@fu-berlin.de>
2014-11-27 18:18:31 +0100

    Simplify flask init process

--- app/__init__.py
+++ app/__init__.py
@@ -9,21 +9,13 @@ def create_app(config=None):
     """Creates the Flask app."""
     app = Flask(__name__)
 
-    configure_app(app)
-    configure_extensions(app)
+    # load config - http://flask.pocoo.org/docs/config/#instance-folders
+    app.config.from_pyfile('../config.cfg', silent=True)
+
+    # init flask-sqlalchemy
+    db.init_app(app)
 
     for blueprint in [main]:
         app.register_blueprint(blueprint)
 
     return app
-
-
-def configure_app(app):
-    # http://flask.pocoo.org/docs/config/#instance-folders
-    app.config.from_pyfile('../default.cfg')
-    app.config.from_pyfile('../config.cfg', silent=True)
-
-
-def configure_extensions(app):
-    # flask-sqlalchemy
-    db.init_app(app)

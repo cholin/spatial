aa51e90f3cfcc1d6182c4f05bb1029b66f0a1ff0
Nico Geyso <nico.geyso@fu-berlin.de>
2014-11-27 18:08:09 +0100

    Load data via ajax

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@
 
 import json
 from datetime import datetime
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, jsonify
 from .models import Station, Measurement
 from .exts import db
 
@@ -10,8 +10,12 @@ main = Blueprint('main', __name__)
 
 @main.route('/')
 def index():
-    param = request.args.get('date', "2014-07-01")
-    date = datetime.strptime(param, "%Y-%m-%d")
-    qry = db.session.query(Measurement).filter(Measurement.date == date)
+    return render_template('index.html')
+
+@main.route('/api/<date>')
+def api(date):
+    date_cmp = datetime.strptime(date, "%Y-%m-%d")
+    qry = db.session.query(Measurement).filter(Measurement.date == date_cmp)
     measurements = [m.to_geojson() for m in qry.all()]
-    return render_template('index.html', geojson=json.dumps(measurements))
+    return jsonify(measurements=measurements)
+

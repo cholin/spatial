b37ec78e6deb97722eff86f7b4ee89fe68881045
Adam Piotr Furmanczuk <adam@debian-lt>
2015-01-08 11:07:32 +0100

    spelling

--- import_forcast.py
+++ import_forcast.py
@@ -2,7 +2,7 @@ import datetime
 import urllib2
 
 
-def download_forcasts():
+def download_forecasts():
     """Downloads forecasts from NOAA GFS 0.5 Grid for Germany.
 
     Downloaded subregion 5.9166670 (west), 14.975 (east), 
@@ -10,7 +10,7 @@ def download_forcasts():
     the reactangle boundry of Germany.
     """
     timestr = datetime.datetime.now().strftime("%Y%m%d")# get forcasts calculated today
-    timestr +='00' # get the forcast calculation from midnight
+    timestr +='00' # get the forecast calculation from midnight
     for forecast in ["00", "12", "24", "36", "48", "72", "96", "120", "144"]:
         response = urllib2.urlopen('http://nomads.ncep.noaa.gov/cgi-bin/filter_gfs_hd.pl?file=gfs.t00z.mastergrb2f{}&subregion=&leftlon=5.916667&rightlon=14.975&toplat=55.052222&bottomlat=47.270108&dir=%2Fgfs.{}%2Fmaster'.format(forecast, timestr))
         data = response.read()
@@ -18,4 +18,4 @@ def download_forcasts():
             fgrib.write(data)
 
 if __name__ == '__main__':
-    download_forcasts()
+    download_forecasts()

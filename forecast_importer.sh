8834bfb93ad088174697ae1ed6b9be0f9e57c9f2
Nico Geyso <nico.geyso@fu-berlin.de>
2015-01-15 00:40:09 +0100

    basic refactoring for forecast import
    this should be refactored further into own import script like for DWD data

--- forecast_importer.sh
+++ forecast_importer.sh
@@ -1,5 +1,36 @@
 #!/bin/bash
 
-# TODO: this needs to be done by the python importer
-for i in 12 24 36 48 72 96 120 144 ; do raster2pgsql -s 4326 -I -b 1 -C -M  /tmp/germany_raster_2015010800_f${i}_TMIN.grib2 raster_20150108_${i}_TMIN | psql -U gisuser -d spatial; done;
-for i in 12 24 36 48 72 96 120 144; do raster2pgsql -s 4326 -I -b 1 -C -M  /tmp/germany_raster_2015010800_f${i}_TMAX.grib2 raster_20150108_${i}_TMAX | psql -U gisuser -d spatial; done;
+PSQL_CMD="psql -U <USERNAME> -d <DATABASE>"
+DATE="2015011300"
+TABLE="forecast"
+
+for i in 12 24 36 48 72 96 120 144 ; do
+  grib_file="/tmp/germany_raster_${DATE}_f${i}_TMIN.grib2"
+  forecast_type="temperature"
+  date_calc="2015-01-12 00:00:00"
+  date_forecast="$i hours"
+  range="min"
+  # -s Assign output raster with specified SRID.
+  # -I Create a GiST index on the raster column.
+  # -b Index (1-based) of band to extract from raster.
+  # -C Apply raster constraints-
+  # -a Append raster(s) to an existing table.
+  # -M Vacuum analyze the raster table.
+  raster2pgsql -s 4326 -I -a -b 1 -C -M "$grib_file" $TABLE  | $PSQL_CMD
+  rid_qry="SELECT rid FROM forecasts ORDER BY rid DESC LIMIT 1"
+  qry="UPDATE forecast
+       SET
+         type='$forecast_type',
+         subtype='$range',
+         date ='$date_calc',
+         interval='$date_forecast'
+       WHERE rid=($rid_qry)";
+  echo $qry | $PSQL_CMD;
+  echo "Added $grib_file"
+done;
+
+#for i in 12 24 36 48 72 96 120 144; do
+#  grib_file="/tmp/germany_raster_${DATE}_f${i}_TMAX.grib2"
+#  raster="raster_20150108_${i}_TMAX"
+#  raster2pgsql -s 4326 -I -b 1 -C -M $grib_file $raster | $PSQL_CMD;
+#done;
--- import_forcast.py
+++ import_forcast.py
@@ -5,16 +5,20 @@ import urllib2
 def download_forecasts():
     """Downloads forecasts from NOAA GFS 0.5 Grid for Germany.
 
-    Downloaded subregion 5.9166670 (west), 14.975 (east), 
-    55.052222 (north), 47.270108 (south) corresponds to
+    Downloaded subregion 5 (west), 15 (east),
+    56 (north), 47 (south) corresponds to
     the reactangle boundry of Germany.
     """
-    timestr = datetime.datetime.now().strftime("%Y%m%d")# get forcasts calculated today
-    timestr +='00' # get the forecast calculation from midnight
+    # get forcasts calculated today
+    timestr = datetime.datetime.now().strftime("%Y%m%d")
+
+    # get the forecast calculation from midnight
+    timestr +='00'
     for forecast in ["12", "24", "36", "48", "72", "96", "120", "144"]:
-	for t in ["TMIN","TMAX"]: # get max and min temperature only
-	    url = 'http://nomads.ncep.noaa.gov/cgi-bin/filter_gfs_hd.pl?file=gfs.t00z.mastergrb2f{}&var_{}=ON&subregion=&leftlon=5.916667&rightlon=14.975&toplat=55.052222&bottomlat=47.270108&dir=%2Fgfs.{}%2Fmaster'.format(forecast, t, timestr)
-	    print url
+        # get max and min temperature only
+        for t in ["TMIN","TMAX"]:
+            url = 'http://nomads.ncep.noaa.gov/cgi-bin/filter_gfs_hd.pl?file=gfs.t00z.mastergrb2f{}&lev_2_m_above_ground=on&var_{}=ON&subregion=&leftlon=5&rightlon=15&toplat=56&bottomlat=47&dir=%2Fgfs.{}%2Fmaster'.format(forecast, t, timestr)
+            print(url)
             response = urllib2.urlopen(url)
             data = response.read()
             with open("/tmp/germany_raster_{}_f{}_{}.grib2".format(timestr, forecast, t), "wb") as fgrib:

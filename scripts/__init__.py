22d8344b204b67ffe34fe366b3c1fad29f56f986
Nico Geyso <nico.geyso@fu-berlin.de>
2015-01-15 16:29:37 +0100

    Refactoring - added doc strings for importer/__init__.py

--- scripts/__init__.py
+++ scripts/__init__.py
@@ -4,6 +4,10 @@ from geoalchemy2.elements import WKTElement
 
 @manager.command
 def resetdb():
+    """
+        Drops all tables in our database, installs postgis extension if
+        necessary and recreates all tables
+    """
     # drop all tables
     db.drop_all()
 
@@ -23,12 +27,15 @@ DEFAULT_PATH= 'pub/CDC/observations_germany/climate/daily/kl/recent/'
 @manager.option('-p', '--path', help='path')
 @manager.option('-l', '--limit', help='limit (maximum of imported stations)')
 def import_weather(host = DEFAULT_HOST, path = DEFAULT_PATH, limit = None):
+    """
+        Imports weather data from 'Deutscher Wetter Dienst' into our database.
+    """
     import json
     from scripts.importer.importer import Importer
     from app.models import Station, Measurement
 
     importer = Importer(host, path)
-    for station in importer.do_import(limit):
+    for i,station in enumerate(importer.do_import(limit)):
         geom = None
         if station.coords is not None:
             geom = WKTElement(station.coords.wkt, srid=4326)
@@ -36,11 +43,13 @@ def import_weather(host = DEFAULT_HOST, path = DEFAULT_PATH, limit = None):
         region = None
         if station.region is not None:
             region = WKTElement(station.region.wkt, srid=4326)
+
         obj = Station(name=station.name, altitude=station.altitude, geom=geom, region=region)
         for m in station.measurements:
             o = Measurement(type='temperature',value=m.temperature,date=m.date)
             obj.measurements.append(o)
+
         db.session.add(obj)
         db.session.commit()
 
-        print("%s: %d" % (station.name, len(station.measurements)))
+        print("\t%d %s: %d" % (i, station.name, len(station.measurements)))
